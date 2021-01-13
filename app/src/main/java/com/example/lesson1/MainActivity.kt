e967ad5a7a574192f507b988723aa083e5857e07
Bulat89 <bylat.anyrovich@gmail.com>
2021-01-13 21:12:21 +0500

    dz 1

--- app/src/main/java/com/example/lesson1/MainActivity.kt
+++ app/src/main/java/com/example/lesson1/MainActivity.kt
@@ -1,14 +1,25 @@
-package com.example.lesson1;
+package com.example.lesson1
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.lesson1.R
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-public class MainActivity extends AppCompatActivity {
+       val button =  findViewById<Button>(R.id.ButtonClick)
+        button.setOnClickListener {
+            (it as Button).text = "Hellow"
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
